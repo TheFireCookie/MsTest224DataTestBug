1ca485c718ab94399382c0d0eaea0faadfd14b4b
MERTENS, Matthias <m.mertens@evs.com>
2021-05-26 14:43:58 +0200

    Small project to reproduce the MSTest bug

---
+++ MsTest224DataTestBug/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MsTest224DataTestBug
+{
+  public class MyStringContainer
+  {
+    public string MyString { get; set; }
+  }
+
+  [TestClass]
+  public class UnitTest1
+  {
+    public static IEnumerable<MyStringContainer[]> GetStrings
+    {
+      get
+      {
+        return new List<MyStringContainer>
+        {
+          new() { MyString = "super string "}
+        }.Select(t => new[] { t });
+      }
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetStrings))]
+    public void TestMethod1(MyStringContainer myStringContainer)
+    {
+    }
+  }
+}
